3b05f79b9a185761d65c898fd6054ee48f6b600e
Andy Barnett <andy.abtec@gmail.com>
2018-01-27 19:15:53 +0000

    Update tvRenameSpecific.js

--- assets/js/tvRenameSpecific.js
+++ assets/js/tvRenameSpecific.js
@@ -7,7 +7,7 @@
 $(function() {
   $.get('https://api.github.com/repos/TV-Rename/tvrename/releases/latest', function(data) {
     $('#download').attr('href', data.assets[0].browser_download_url);
-    $('.btn btn--light-outline btn--large').attr('href', data.assets[0].browser_download_url);
+    $('.btn--large').attr('href', data.assets[0].browser_download_url);
     $('.page__lead').html($('.page__lead').html().substring(0,164) + 'v ' + data.tag_name + '<br />');
   });
 });
