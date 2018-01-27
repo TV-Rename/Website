15dec56e2e6d39fcc8dfd218191bab3616b551ad
Andy-ABTec <andy:ab-tec.co.uk>
2018-01-27 18:33:52 +0000

    Testing jQuery tweaked url's

--- _pages/home.md
+++ _pages/home.md
@@ -6,7 +6,7 @@ header:
   overlay_image: assets/images/pexels-photo-333984.jpeg	
   overlay_filter: rgba(20, 20, 20, 0.5)
   cta_label: "<i class='fa fa-download'></i> Download Now"
-  cta_url: "https://github.com/TV-Rename/tvrename/releases/download/2.3.2/TVRename-2.3.2.exe"
+  cta_url: "https://github.com/TV-Rename/tvrename/releases"
   caption:
 excerpt: "**Television - Sorted...**<br /><small><i>Organise your TV Shows with<br />the Swiss-Army-Knife of<br />media management tools.</i></small><br />Latest Release: <br />"
 feature_row:
--- assets/js/tvRenameSpecific.js
+++ assets/js/tvRenameSpecific.js
@@ -2,26 +2,12 @@
  TV Rename Specific JavaScript
 ====================================*/
 
-// Update masthead download link
+// Update download links with latest .exe and page lead class with latest version
 
 $(function() {
   $.get('https://api.github.com/repos/TV-Rename/tvrename/releases/latest', function(data) {
     $('#download').attr('href', data.assets[0].browser_download_url);
-  });
-});
-
-// Update page__lead class with TV Rename Version
-
-$(function() {
-  $.get('https://api.github.com/repos/TV-Rename/tvrename/releases/latest', function(data) {
-	$('.page__lead').html($('.page__lead').html().substring(0,164) + 'v ' + data.tag_name + '<br />');
-  });
-});
-
-// Update button download link
-
-$(function() {
-  $.get('https://api.github.com/repos/TV-Rename/tvrename/releases/latest', function(data) {
     $('.btn btn--light-outline btn--large').attr('href', data.assets[0].browser_download_url);
+	$('.page__lead').html($('.page__lead').html().substring(0,164) + 'v ' + data.tag_name + '<br />');
   });
 });
