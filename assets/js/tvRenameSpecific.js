4f6590ad3793fb303029ebe3ec52826631901fb4
Andy-ABTec <andy:ab-tec.co.uk>
2018-02-06 17:28:11 +0000

    cleaning up

--- assets/js/tvRenameSpecific.js
+++ assets/js/tvRenameSpecific.js
@@ -8,20 +8,10 @@
 $(function() {
 	$.get('https://api.github.com/repos/TV-Rename/tvrename/releases/latest', function(data) {
 		$('#download').attr('href', data.assets[0].browser_download_url);
-<<<<<<< HEAD
-		$('.btn.btn--light-outline.btn--large').attr('href', data.assets[0].browser_download_url);
-=======
 		$('.btn.btn--light-outline.btn--large').attr('href', data.assets[0].browser_download_url)
->>>>>>> cf6dc9b25b9fab6116a780a5db34ce2d556db8e9
 
 		if ($('.page__lead')[0]) {
 			$('.page__lead').html($('.page__lead').html().substring(0,164) + 'v ' + data.tag_name + '<br />');
 		};
 	});
-});
-
-// Insert "The Manual" submenu list into the mix //
-
-$.get("http://localhost:4000/assets/html/manual-chapters.html", function (response) {
-	$( "#the-manual" ).after(response)
 });
